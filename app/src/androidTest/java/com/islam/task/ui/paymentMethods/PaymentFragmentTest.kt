7c15efcb723ee319231ace00bcc76eee1cba313e
islam.ar <islam.ahmed@approcks.com>
2021-08-05 17:51:57 +0200

    Start Test Fragment

---
+++ app/src/androidTest/java/com/islam/task/ui/paymentMethods/PaymentFragmentTest.kt
@@ -0,0 +1,26 @@
+package com.islam.task.ui.paymentMethods
+
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.fragment.app.viewModels
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.islam.task.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class PaymentFragmentTest {
+
+    @Test
+    fun activeTaskDetails_DisplayedInUi() {
+
+        val scenario = launchFragmentInContainer<PaymentFragment>(Bundle(), R.style.Theme_MyTask)
+
+
+        Thread.sleep(7000)
+
+
+    }
+}
