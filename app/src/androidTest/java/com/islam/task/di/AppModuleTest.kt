646fb3de48ec121bfa3a8b81225f2a0dc7abc109
islam.ar <islam.ahmed@approcks.com>
2021-08-06 22:54:02 +0200

    Add more instrument unit testing

--- app/src/androidTest/java/com/islam/task/di/AppModuleTest.kt
+++ app/src/androidTest/java/com/islam/task/di/AppModuleTest.kt
@@ -1,26 +1,11 @@
 package com.islam.task.di
 
-import android.content.Context
-import com.google.gson.GsonBuilder
-import com.islam.task.data.network.MyTaskApi
-import com.islam.task.data.network.internet.ConnectivityInterCeptor
-import com.islam.task.data.network.internet.ConnectivityInterCeptorImpl
-import com.islam.task.data.repositories.DefaultPaymentRepository
 import com.islam.task.data.repositories.PaymentRepository
-import com.islam.task.generalUtils.Utils
 import com.islam.task.repositories.FakePaymentMethodRepoForUI
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
--- app/src/androidTest/java/com/islam/task/ui/paymentMethods/PaymentFragmentTest.kt
+++ app/src/androidTest/java/com/islam/task/ui/paymentMethods/PaymentFragmentTest.kt
@@ -1,11 +1,13 @@
 package com.islam.task.ui.paymentMethods
 
 import android.os.Bundle
-import androidx.fragment.app.testing.launchFragmentInContainer
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.PerformException
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -14,10 +16,14 @@ import com.islam.task.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.Matcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.Description
 import org.junit.runner.RunWith
+
 
 @ExperimentalCoroutinesApi
 @MediumTest
@@ -29,10 +35,41 @@ class PaymentFragmentTest {
     var hiltRule = HiltAndroidRule(this)
 
     @Before
-    fun setup(){
+    fun setup() {
 
         launchFragmentInHiltContainer<PaymentFragment>(Bundle(), R.style.Theme_MyTask)
 
+    }
+
+    @Test
+    fun check_isListVisible_onFragmentLaunch() {
+
+        onView(withId(R.id.list)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun check_isItemTextDisplayed() {
+
+        onView(withId(R.id.list)).check(matches(atPosition(0, hasDescendant(withText("VISA")))))
+
+    }
+
+    private fun atPosition(position: Int, itemMatcher: Matcher<View?>): Matcher<View?> {
+        return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+
+            override fun describeTo(description: org.hamcrest.Description?) {
+                description?.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+        }
     }
 
     @Test
@@ -57,4 +94,5 @@ class PaymentFragmentTest {
                 )
             )
     }
+
 }
